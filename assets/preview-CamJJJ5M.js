a42ee1ac00d139ffccc74dce266077a4bf4dde14
sachin <sachinbhattarai55@gmail.com>
2024-03-06 00:03:20 +0545

    Updates

---
+++ assets/preview-CamJJJ5M.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-Lxs7co4U.js","./iframe-CrOcdhnY.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-Bym-zv4b.js","./index-Bw8VTzHM.js","./_getPrototype-C0MsqmOH.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as t}from"./iframe-CrOcdhnY.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-Lxs7co4U.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7]),import.meta.url);return new r}}};export{_ as parameters};
