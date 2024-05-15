23bd3070af048f429689960b14119726918bd514
sachin <sachinbhattarai55@gmail.com>
2024-05-15 22:57:20 +0545

    Updates

---
+++ assets/Button.stories-B45POYI1.js
@@ -0,0 +1,5 @@
+import{B as e}from"./Button-8wynudww.js";import"./jsx-runtime-CKrituN3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./styled-components.browser.esm-50nrqITv.js";const c={title:"Atomic/Button",component:e,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{}},r={args:{variant:"primary"}};var t,a,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    variant: "primary"
+  }
+}`,...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const d=["Primary"];export{r as Primary,d as __namedExportsOrder,c as default};
---
+++ assets/Typography.stories-CrAYuc1l.js
@@ -0,0 +1,5 @@
+import{T as a}from"./index-DvvV-vcL.js";import"./jsx-runtime-CKrituN3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./styled-components.browser.esm-50nrqITv.js";const i={title:"Atomic/Typography",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{}},r={args:{children:"This is a typography component."}};var o,t,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: "This is a typography component."
+  }
+}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const d=["Text"];export{r as Text,d as __namedExportsOrder,i as default};
